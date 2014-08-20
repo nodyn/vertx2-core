4b94bfbc3ae3cc266a395475f49f5b3002a05ff7
Bob McWhirter <bob@mcwhirter.org>
2014-08-20 10:31:42 -0400

    Add some inbound and outbound conversions of messages on the eventbus.

--- eventbus.js
+++ eventbus.js
@@ -27,8 +27,8 @@ function HandlerRegistration(address, handler) {
   this._handle = new io.nodyn.handle.HandleWrap( process._process, true );
   Handle.call( this, this._handle );
 
-  this.address = address;
-  this.handler = handler;
+  this._address = address;
+  this._handler = deliverMessage.bind(handler);
   this.register();
 }
 
@@ -36,13 +36,93 @@ util.inherits( HandlerRegistration, Handle );
 
 HandlerRegistration.prototype.register = function() {
   this.ref();
-  __vertx.eventBus().registerHandler( this.address, this.handler );
+  __vertx.eventBus().registerHandler( this._address, this._handler );
 };
 
 HandlerRegistration.prototype.unregister = function() {
-  __vertx.eventBus().unregisterHandler( this.address, this.handler );
+  __vertx.eventBus().unregisterHandler( this._address, this.handler );
   this.unref();
 };
+
+function deliverMessage(m) {
+  var message = new Message(m);
+  this( message );
+}
+
+// ----------------------------------------
+// Message
+// ----------------------------------------
+
+function Message(message) {
+  this._message = message;
+  this.address = message.address();
+}
+
+Message.prototype.reply = function(body) {
+  this._message.reply( convertOutbound( body ) );
+}
+
+Object.defineProperty( Message.prototype, 'body', {
+  get: function() {
+    return convertInbound(this._message.body());
+  },
+  enumerable: true,
+});
+
+
+// ----------------------------------------
+// inbound conversions
+// ----------------------------------------
+
+function convertInbound(body) {
+  if (typeof body == 'number' || typeof body == 'string' || typeof body == 'boolean') {
+    return body;
+  }
+
+  if (body instanceof org.vertx.java.core.json.JsonObject ) {
+    return convertInboundJson(body);
+  }
+
+  if (body instanceof org.vertx.java.core.json.JsonArray ) {
+    return convertInboundJson(body);
+  }
+
+  return {};
+}
+
+function convertInboundJson(obj) {
+  return JSON.parse( obj.encode() );
+}
+
+// ----------------------------------------
+// outbound conversions
+// ----------------------------------------
+
+function convertOutbound(body) {
+  if (typeof body == 'number' || typeof body == 'string' || typeof body == 'boolean') {
+    return body;
+  }
+
+  if (typeof body == 'object' ) {
+    return convertOutboundJsonObject(body);
+  }
+
+  if (typeof body == 'function') {
+    return convertOutbound( body() );
+  }
+
+  throw new Error( "Cannot send on eventbus: " + body );
+}
+
+function convertOutboundJsonObject(body) {
+  var str = JSON.stringify(body);
+  if ( str[0] == '[' ) {
+    return new org.vertx.java.core.json.JsonArray( str );
+  } else {
+    return new org.vertx.java.core.json.JsonObject( str );
+  }
+}
+
 
 // ----------------------------------------
 // vertx.eventBus()
@@ -54,8 +134,12 @@ eventbus.publish = function(address, message) {
   __vertx.eventBus().publish( address, message );
 };
 
-eventbus.send = function(address, message) {
-  __vertx.eventBus().send( address, message );
+eventbus.send = function(address, message, replyHandler) {
+  if ( replyHandler ) {
+    __vertx.eventBus().send( address, convertOutbound( message ), deliverMessage.bind(replyHandler) );
+  } else {
+    __vertx.eventBus().send( address, convertOutbound( message ) );
+  }
 };
 
 eventbus.register = function(address, handler) {
